dabd3adb72abef223a112f0b7ce68a1a562130c3
Mison Caamwang <mison.caamwang@hotmail.com>
2019-09-19 01:22:09 +1000

    added google maps api

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 /*
-- Search Movies and return a list.
-- Click the movie poster and show the location and time it's playing, if it's not then show nothing. 
-
-- Popular movies playing in your city. (Use google maps api)
-
+- Search Movies.
+- Show movie theater near user's location.
 */
+
+/*OMDB API*/
+let search;
 $(document).ready(function(){
     $('#search-form').on('submit',function(e){
         e.preventDefault();
-        let search = $('#movie-name').val();
+        search = $('#movie-name').val();
         $('#movie-name').val('');
-        console.log(search);
+        //console.log(search.length);
 
         getMovies(search);
-        
+        initMap();
+    })
+
+    //Modal
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
     })
 })
 
@@ -25,43 +30,124 @@ function getMovies(searchParam) {
     //Pagination feature. 
     $.getJSON('http://www.omdbapi.com/?apikey=576f5cb7&s='+ searchParam /* &page=1 */, function(data){
         //console.log(data.Search[0]);
-        console.log(data.Search);
+        //console.log(data.Search);
         let movieList = data.Search;
 
-        $('#movie-list').html('');
+        if (search.length < 3) {
+            console.log('To many search results');
+            alert('Too many results! Movie name needs to be longer than 2 letters.')
 
-        for (let i = 0; i < movieList.length; i++) {
-            //console.log(movieList[i].Title)
-    
-            $('#movie-list').append(
-                `
-                <div class="col-md-4 mb-5 movie-grid-item">
-                    <div class="card" style="width: 18rem;">
-                        <img src="${movieList[i].Poster}" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">${movieList[i].Title}</h5>
-                            <h6 class="card-title">${movieList[i].Year}</h6>
-                            <a href="https://www.imdb.com/title/${movieList[i].imdbID}/" class="card-link" target="_blank">More Info</a>
-                            <p class="card-text"></p>
+        } else {
+            $('#movie-list').html('');
 
-                            <button type="button" class="btn btn-primary" id="show-time-btn">View Show Times</button>
+            for (let i = 0; i < movieList.length; i++) {
+                //console.log(movieList[i].Title)
+        
+                $('#movie-list').append(
+                    `
+                    <div class="col-lg-4 mb-5 movie-grid-item">
+                        <div class="card" style="width: 18rem;">
+                            <img src="${movieList[i].Poster}" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">${movieList[i].Title}</h5>
+                                <h6 class="card-title">${movieList[i].Year}</h6>
+                                <a href="https://www.imdb.com/title/${movieList[i].imdbID}/" class="card-link" target="_blank">More Info</a>
+                                <p class="card-text"></p>
+                                <a class="btn btn-primary" id="show-time-btn" href="https://www.imdb.com/showtimes/title/${movieList[i].imdbID}" role="button" target="_blank">View Show Times</a>
+                                
+                            </div>
                         </div>
                     </div>
-                </div>
-                `
-            )
+                    `
+                )
+            }
         }
-        //Use maps API
-        showTime();
-    })  
-    
+    })   
 }
 
-function showTime() {
-    $('#show-time-btn').on('click', function(e){
-        e.preventDefault();
-        //alert('It worked');
-    }) 
+/*GOOGLE MAPS API*/
+
+//Global variables
+var map;
+var service;
+var infoWindow;
+var pos;
+//Main map function
+function initMap() {
+    var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+    infoWindow = new google.maps.InfoWindow;
+
+    //Create a map object 
+    map = new google.maps.Map(document.getElementById('map'), {center: sydney, zoom: 12});
+
+    // Geolocation API
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            //console.log(position);
+            pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            }
+            
+            //infoWindow.open(map);
+            //Add a green marker for user location.
+            var image = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+            var marker = new google.maps.Marker({
+                position: pos, 
+                map: map,
+                title: 'User Location',
+                icon: image 
+            });
+
+            //Infowindow displays when green marker is clicked
+            marker.addListener('click', function() {
+                infoWindow.open(map, marker);
+            });
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Your Location');
+            
+            map.setCenter(pos);
+            marker.setMap(map);
+
+            //Search param 
+            var request = {
+                //Test for perth:
+                //{lat: -31.953512, lng: 115.8605}, 
+                location: {lat: position.coords.latitude, lng: position.coords.longitude},
+                radius: '10000',
+                type: ['movie_theater']
+            };
+        
+            service = new google.maps.places.PlacesService(map);
+            service.nearbySearch(request, callback);
+
+        })
+    } else {
+        console.log('Geolocation not supported by your browser');
+    }      
 }
 
+//Creates red markers for movie theatres 
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            createMarker(results[i]);
+            //console.log(results[i]);
+        }
+    }
+}
 
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(place.name);
+        infoWindow.open(map, this);
+    });
+}
